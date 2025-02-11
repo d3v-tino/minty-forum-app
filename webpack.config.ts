6c436175c55b3f97fd542afdc0c0e418708ea527
d3v-tino <zaad1012@outlook.com>
2025-02-12 00:59:27 +0100

    client set up

---
+++ webpack.config.ts
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript"
+            ]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"), // ✅ Updated path
+      filename: "index.html"
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: "./dist",
+    hot: true,
+    open: true,
+    port: 3000,
+    client: {
+      logging: "none",
+    }
+  },
+};
